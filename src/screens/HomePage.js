0c596e693d189b68c0d4c4eb178c355451107bb4
Xyler7 <kutaydemir4539@gmail.com>
2025-07-16 23:45:18 +0300

    Updating content of spesific datas

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,67 +1,106 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View, TextInput } from 'react-native'
 import React from 'react'
 import { collection, addDoc, getDocs, doc, deleteDoc, updateDoc } from "firebase/firestore"; 
 import { db } from '../../firebaseConfig';
 import { CustomButton } from '../components';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const HomePage = () => {
 
   const [data, setData] = useState([])
+  const[isSaved, setIsSaved] =useState(false)
+  const [updateTheData, setUpdateTheData] = useState('')
 
+  console.log(isSaved)
   console.log( "data: ", data )
+
+  useEffect(() => {
+    getData()
+  }, [isSaved])
 
   //SEND DATA TO FIREBASE
   const sendData = async() =>{
     try {
-  const docRef = await addDoc(collection(db, "reactNativeLessons"), {
-    title: "Zero to Hero",
-    content: "React Native Tutorial for Beginners",
-    lesson: 95 
-  });
-  console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
+      const docRef = await addDoc(collection(db, "reactNativeLessons"), {
+        title: "Zero to Hero",
+        content: "React Native Tutorial for Beginners",
+        lesson: 95 
+      });
+      console.log("Document written with ID: ", docRef.id),
+      getData();
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
   }
 
   //GET DATA FROM FIREBASE
 
   const getData= async() =>{
 
-    const querySnapshot = await getDocs(collection(db, "reactNativeLessons"));
-    querySnapshot.forEach((doc) => {
-      //console.log(`${doc.id} => ${doc.data()}`);
-      setData([...data, doc.data() ])
-    });
+    const allData = []
+
+    try {
+
+      const querySnapshot = await getDocs(collection(db, "reactNativeLessons"));
+      querySnapshot.forEach((doc) => {
+        //console.log(`${doc.id} => ${doc.data()}`);
+        allData.push({...doc.data(), id :doc.id})
+      });
+      setData(allData) 
+    
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   //DELETE DATA FROM DATABASE
-  const deleteData = async() => {
-    await deleteDoc(doc(db, "reactNativeLessons", "DcUEVh18XGndqkZj1HRv"));
-  }
+  const deleteData = async(value) => {
+    try {
+      await deleteDoc(doc(db, "reactNativeLessons", value));
+      console.log("Delete Succesfull!")
+      
+    } catch (error) {
+      console.log(error)
+    }
+    }
 
 
 //UPDATE DATA FROM FIREBASE
-  const updateData = async() => {
+  const updateData = async(value) => {
     try {
-      const lessonData = doc(db, "reactNativeLessons", "PELkAyOETKK1wT5LijyX");
+
+      const lessonData = doc(db, "reactNativeLessons", value);
       await updateDoc(lessonData, {
-        lesson: 35
-});    
+        content: updateTheData,
+      });    
     } catch (error) {
       console.log(error)
     }
   }
 
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>{data[0]?.title}</Text>
-      <Text>{data[0]?.content}</Text>
-      <Text>{data[0]?.lesson}</Text>
-      <Text>{data[1]?.title}</Text>
-      <Text>{data[1]?.content}</Text>
-      <Text>{data[1]?.lesson}</Text>
+    <View style={styles.container}>
+      
+    <TextInput
+      value ={updateTheData}
+      onChangeText={setUpdateTheData}
+      placeholder='Enter Your Data: '
+      style={{borderWidth:1, width:'50%', paddingVertical:5, textAlign:'center', marginBottom:30}}
+    />
+
+      {data.map((value,index) => {
+        return(
+          <Pressable 
+          onPress={()=> [updateData(value.id), setIsSaved(isSaved === false ? true : false)]} 
+          key={index}>
+            <Text>{index}</Text>
+            <Text>{value?.id}</Text>
+            <Text>{value.title}</Text>  
+            <Text>{value.content}</Text>  
+            <Text>{value.lesson}</Text>                          
+          </Pressable>
+        )
+      })}
       
       <CustomButton
       buttonText={"Save"}
@@ -97,9 +136,17 @@ const HomePage = () => {
 
     </View>
   );
-};
+}
 
 
 export default HomePage
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor:'#fbfbfb'
+  }
+});
